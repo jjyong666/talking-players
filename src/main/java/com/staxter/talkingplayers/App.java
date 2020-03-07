a5b8ec35cc3be48b008b3a586e13b603d96b16e0
jj <jjyong90@gmail.com>
2020-03-07 14:37:21 +0100

    Initial commit

---
+++ src/main/java/com/staxter/talkingplayers/App.java
@@ -0,0 +1,19 @@
+package com.staxter.talkingplayers;
+
+/**
+ * The type App.
+ */
+public class App {
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+  }
+
+}
