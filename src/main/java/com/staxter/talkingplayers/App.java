1b7def5282a927f43358ab6ee9c29bea14d4c6f6
jj <jjyong90@gmail.com>
2020-03-07 15:40:44 +0100

    Add starting logic of the application

--- src/main/java/com/staxter/talkingplayers/App.java
+++ src/main/java/com/staxter/talkingplayers/App.java
@@ -1,5 +1,13 @@
 package com.staxter.talkingplayers;
 
+import com.staxter.talkingplayers.model.Player;
+import com.staxter.talkingplayers.service.manager.TalkManager;
+import com.staxter.talkingplayers.service.manager.TalkManagerMediator;
+import com.staxter.talkingplayers.service.message.receiver.MessageReceiver;
+import com.staxter.talkingplayers.service.message.receiver.MessageReceiverImpl;
+import com.staxter.talkingplayers.service.message.sender.MessageSender;
+import com.staxter.talkingplayers.service.message.sender.MessageSenderImpl;
+
 /**
  * The type App.
  */
@@ -12,7 +20,17 @@ public class App {
    */
   public static void main(String[] args) {
 
-    System.out.println("Hello World!");
+    TalkManager manager = new TalkManagerMediator();
+    MessageSender sender = new MessageSenderImpl(manager);
+    MessageReceiver receiver = new MessageReceiverImpl(sender);
+
+    Player initiator = new Player("initiator", sender, receiver);
+    manager.registerPlayer(initiator);
+
+    Player replier = new Player("replier", sender, receiver);
+    manager.registerPlayer(replier);
+
+    initiator.sendMessage("oh", replier);
 
   }
 
