254c0274ff342492432644b6c63d38381001287c
jj <jjyong90@gmail.com>
2020-04-05 19:04:50 +0200

    Refactor talkManager to send a close command when a player reaches limit

--- server/src/main/java/com/staxter/talkingplayers/server/domain/manager/TalkManagerMediator.java
+++ server/src/main/java/com/staxter/talkingplayers/server/domain/manager/TalkManagerMediator.java
@@ -4,6 +4,7 @@ import com.staxter.talkingplayers.server.domain.config.MessageLimitConfig;
 import com.staxter.talkingplayers.server.domain.model.Player;
 import com.staxter.talkingplayers.shared.dto.PlayerMessage;
 import com.staxter.talkingplayers.shared.dto.ServerMessage;
+import com.staxter.talkingplayers.shared.dto.command.CloseCommandDto;
 
 /**
  * The type Talk manager mediator.
@@ -24,11 +25,21 @@ public class TalkManagerMediator implements TalkManager {
     @Override
     public void sendMessage(Player from, Player to, String message) {
         int messageLimit = config.getMessageLimit();
+        if (from.getSentCount() >= messageLimit && from.getReceivedCount() >= messageLimit) {
+            System.out.println("Message limit has been reached!");
+            from.receiveMessage(new ServerMessage("Your session is over"));
+            to.receiveMessage(new ServerMessage("Your session is over"));
+            from.receiveMessage(new ServerMessage(CloseCommandDto.COMMAND));
+            to.receiveMessage(new ServerMessage(CloseCommandDto.COMMAND));
+            return;
+        }
         if (from.getSentCount() >= messageLimit) {
+            System.out.println("Player reached sent limit: " + from.getName());
             from.receiveMessage(new ServerMessage("You cannot send more messages"));
             return;
         }
         if (to.getReceivedCount() >= messageLimit) {
+            System.out.println("Player reached received limit: " + to.getName());
             from.receiveMessage(new ServerMessage(to.getName() + " cannot receive more messages"));
             return;
         }
